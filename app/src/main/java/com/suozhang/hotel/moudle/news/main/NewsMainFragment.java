eb152e18c2abd31e1bd75605f6d8946a612b8d79
JamesMarts <1440919311@qq.com>
2017-03-21 18:27:08 +0800

    添加基础工具类

--- app/src/main/java/com/suozhang/hotel/moudle/news/main/NewsMainFragment.java
+++ app/src/main/java/com/suozhang/hotel/moudle/news/main/NewsMainFragment.java
@@ -1,6 +1,7 @@
 package com.suozhang.hotel.moudle.news.main;
 
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -13,8 +14,10 @@ import com.suozhang.hotel.injector.module.NewsMainModule;
 import com.suozhang.hotel.local.table.NewsTypeInfo;
 import com.suozhang.hotel.moudle.base.BaseFragment;
 import com.suozhang.hotel.moudle.base.IRxBusPresenter;
+import com.suozhang.hotel.moudle.news.list.NewsListFragment;
 import com.suozhang.hotel.rxbus.event.ChannelEvent;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -37,7 +40,14 @@ public class NewsMainFragment extends BaseFragment<IRxBusPresenter> implements I
 
     @Override
     public void loadData(List<NewsTypeInfo> checkList) {
+        List<Fragment> fragments = new ArrayList<>();
+        List<String> title = new ArrayList<>();
+        for (NewsTypeInfo n : checkList) {
+            fragments.add(NewsListFragment.getInstance(n.getTypeId()));
+            title.add(n.getName());
+        }
 
+        mPagerAdapter.setItem(title, fragments);
     }
 
     @Override
@@ -64,30 +74,41 @@ public class NewsMainFragment extends BaseFragment<IRxBusPresenter> implements I
         mPresenter.registerRxBus(ChannelEvent.class, new Action1<ChannelEvent>() {
             @Override
             public void call(ChannelEvent channelEvent) {
-          handleChannelEvent(channelEvent);
+                handleChannelEvent(channelEvent);
             }
         });
     }
 
     @Override
     protected void updateViews() {
-
+        mPresenter.getData();
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
+
+
     }
 
-    private void  handleChannelEvent(ChannelEvent channelEvent) {
-       switch (channelEvent.eventType){
-           case ChannelEvent.ADD_EVENT:
-             //  mPagerAdapter.addItem(channelEvent.newsInfo.getName(),NewsListFragment.getInstance());
-               break;
-           case ChannelEvent.DEL_EVENT:
-               break;
-           case ChannelEvent.SWAP_EVENT:
-               break;
-       }
+    private void handleChannelEvent(ChannelEvent channelEvent) {
+        switch (channelEvent.eventType) {
+            case ChannelEvent.ADD_EVENT:
+                mPagerAdapter.addItem(channelEvent.newsInfo.getName(), NewsListFragment.getInstance(channelEvent.newsInfo.getTypeId()));
+                break;
+            case ChannelEvent.DEL_EVENT:
+                mViewPager.setCurrentItem(0);
+                mPagerAdapter.delItem(channelEvent.newsInfo.getName());
+                break;
+            case ChannelEvent.SWAP_EVENT:
+                mPagerAdapter.changeItem(channelEvent.fromPos, channelEvent.toPos);
+                break;
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mPresenter.unRegisterRxBus();
     }
 }
